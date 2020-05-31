3f9f32565fa238ed0f6060dcff09cca8f25edf58
Davide Cristini <davcri91@gmail.com>
2020-05-31 12:08:49 +0200

    Update size

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
-  gameWidth: 240*4,
-  gameHeight: 160*4,
+  gameWidth: 960, // 240*4,
+  gameHeight: 640, // 160*4,
   localStorageName: 'phaseres6webpack',
   webfonts: ['Bangers', 'Quantico', 'Press Start 2P']
 }
